6340a5bbf7a7f8ba8b9260e158c90299016502bb
DRegan-dev <damienreganmusic@gmail.com>
2024-02-04 00:09:23 +0000

    added js for continue button and styling for transition to quiz section

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
-const main = document.querySelector('.main')
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section')
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -9,6 +11,13 @@ startBtn.onclick = () => {
 }
 
 exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
 
