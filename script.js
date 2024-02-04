24d3cc01cfc9668ba4ddfd75ad3c0f7ba48e5ded
DRegan-dev <damienreganmusic@gmail.com>
2024-02-04 21:59:23 +0000

    added quizBox functionality to continueBtn event handler

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
-const quizSection = document.querySelector('.quiz-section')
+const quizBox = document.querySelector('.quiz-box')
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -20,5 +20,6 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
 
 }
