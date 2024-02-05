73b119122e0d6d47ca261f810ef03edb67e83363
DRegan-dev <damienreganmusic@gmail.com>
2024-02-05 01:09:38 +0000

    edited answers in questions.js

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ let questions = [
     {
     numb: 1,
     question: "What 1949 science fiction book by author George Orwell describes a dystopian world in the future?",
-    answer: "C",
+    answer: "C. 1984",
     options: [
         "A. Bladerunner", 
         "B. The Stranger", 
@@ -13,7 +13,7 @@ let questions = [
   {
     numb: 2,
     question: "Which Italian town is the setting for Shakespeares Romeo and Juliet?",
-    answer: "D",
+    answer: "D. Verona",
     options: [
         "A. Rome", 
         "B. Milan", 
@@ -24,7 +24,7 @@ let questions = [
   {
     numb: 3,
     question: "Which globally-dreaded disease did the World Health Organization declared eradicated in 1980?",
-    answer: "B",
+    answer: "B. Small Pox",
     options: [
         "A. Sars", 
         "B. Small Pox", 
@@ -35,7 +35,7 @@ let questions = [
   {
     numb: 4,
     question: "Who was the first “American Idol” winner?",
-    answer: "D",
+    answer: "D. Kelly Clarkson",
     options: [
         "A. Katy Perry", 
         "B. Celine Dion", 
@@ -45,7 +45,7 @@ let questions = [
   {
     numb: 5,
     question: "What movie is 'you had me at hello' from?",
-    answer: "D",
+    answer: "D. Jerry Maguire",
     options: [
         "A. Billy Elliot", 
         "B. Lil Nicky", 
@@ -56,7 +56,7 @@ let questions = [
   {
     numb: 6,
     question: "Which of the six main characters on the TV show “Friends” never got married?",
-    answer: "C",
+    answer: "C. Joey",
     options: [
         "A. Ross", 
         "B. Rachel", 
@@ -67,7 +67,7 @@ let questions = [
   {
     numb: 7,
     question: "What species of fish is Nemo?",
-    answer: "B",
+    answer: "B. Clownfish",
     options: [
         "A. Pufferfish", 
         "B. Clownfish", 
@@ -78,7 +78,7 @@ let questions = [
   {
     numb: 8,
     question: "Where did the 2000 Summer Olympics take place?",
-    answer: "B",
+    answer: "B. Australia",
     options: [
         "A. Greece", 
         "B. Australia", 
@@ -89,7 +89,7 @@ let questions = [
   {
     numb: 9,
     question: "What is an eight-sided shape called?",
-    answer: "D",
+    answer: "D. Octagon",
     options: [
         "A. Hexagon", 
         "B. Rectangle", 
@@ -99,7 +99,7 @@ let questions = [
   {
     numb: 10,
     question: "Where is the worlds largest Starbucks?",
-    answer: "C",
+    answer: "C. Chicago",
     options: [
         "A. New York", 
         "B. Brisbane", 
@@ -110,7 +110,7 @@ let questions = [
   {
     numb: 11,
     question: "What is a single strand of spaghetti called?",
-    answer: "A",
+    answer: "A. Spaghetto",
     options: [
         "A. Spaghetto", 
         "B. Noodle", 
@@ -121,7 +121,7 @@ let questions = [
   {
     numb: 12,
     question: "Which NBA team plays its home games at Madison Square Garden?",
-    answer: "C",
+    answer: "C. Knicks",
     options: [
         "A. Lakers", 
         "B. Bulls", 
@@ -132,7 +132,7 @@ let questions = [
   {
     numb: 13,
     question: "What color is a giraffe’s tongue",
-    answer: "D",
+    answer: "D. Purple",
     options: [
       "A. Black", 
       "B. Yellow", 
@@ -143,7 +143,7 @@ let questions = [
   {
     numb: 14,
     question: "Who wrote “The Little Mermaid”? ",
-    answer: "B",
+    answer: "B. Hans C. Anderson",
     options: [
         "A. Walt Disney", 
         "B. Hans C. Anderson", 
@@ -154,7 +154,7 @@ let questions = [
   {
     numb: 15,
     question: "How many ghosts show up in “A Christmas Carol”?",
-    answer: "C",
+    answer: "C. Four",
     options: [
         "A. Three", 
         "B. Two", 
