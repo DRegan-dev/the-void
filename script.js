e5a2b6250c34d6442e5a779e0cd34e83487fd9bb
DRegan-dev <damienreganmusic@gmail.com>
2024-02-05 11:57:17 +0000

    added js to provide functionality for try again button, ressetting values to zero and returning user to beginning of quiz

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -26,6 +28,22 @@ continueBtn.onclick = () => {
 
     showQuestions(0);
     questionCounter(1);
+    headerScore();
+
+}
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
     headerScore();
 
 }
